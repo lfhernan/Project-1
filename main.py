3f3ac8d02bf28d8a728af3a2402a70cbae130cf9
Luis Hernandez <lfhernan@fiu.edu>
2020-10-05 14:29:33 -0400

    Final version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 '''
-Luis F. Hernandez JR
-5163353
+Name: Luis F. Hernandez JR
+PID: 5163353
+Class: COP 4813 Web Application Programming
+Assignment: Project 1 - Streamlit, NYTimes API, JSON Documents
 '''
 
 import streamlit as st
@@ -10,6 +12,10 @@ topic_options = ["", "arts", "automobiles", "books", "business", "fashion", "foo
                  "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics",
                  "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine",
                  "travel", "upshot", "us", "world"]
+
+article_types = ["", "shared", "emailed", "viewed"]
+
+article_times = ["", "1", "7", "30"]
 
 st.title("COP 4813 - Web Application Programming")
 st.header("Project 1")
@@ -22,7 +28,7 @@ topic = st.selectbox(label="Topic", options=topic_options)
 
 if name and topic:
     st.write("Hello " + name + "! You've selected the " + topic + " topic.")
-    freqDist = st.checkbox(label="Click here to see frequency distribution of the 10 most common words")
+    freqDist = st.checkbox(label="Click here to see frequency distribution of the 10 most common words in your topic")
 
     if freqDist:
         word_dist = main_functions.freq_dist(topic)
@@ -32,5 +38,17 @@ if name and topic:
     word_cloud = st.checkbox(label="Click here to generate a word cloud")
 
     if word_cloud:
-        main_functions.create_wordcloud(topic)
-        st.image("Images/wordcloud.png", caption="Wordcloud of most common words", use_column_width=True)
+        main_functions.create_wordcloud_top_stories(topic)
+        st.image("Images/top_articles_wordcloud.png", caption="Wordcloud of the most common words in the " + topic + " topic.", use_column_width=True)
+
+st.subheader("Part B - Most Popular API")
+
+st.write("Please select if you would like to see the most shared, emailed, or viewed articles")
+article = st.selectbox(label="Preferred set of articles", options=article_types)
+
+st.write("Please select the time period of the most popular articles (1, 7, or 30 days)")
+times = st.selectbox(label="Time Period (days)", options=article_times)
+
+if article and times:
+    main_functions.create_wordcloud_most_popular(article, times)
+    st.image("Images/most_popular_wordcloud.png", caption="Wordcloud of " + article + " articles", use_column_width=True)
--- main_functions.py
+++ main_functions.py
@@ -1,6 +1,8 @@
 '''
-Luis F. Hernandez JR
-5163353
+Name: Luis F. Hernandez JR
+PID: 5163353
+Class: COP 4813 Web Application Programming
+Assignment: Project 1 - Streamlit, NYTimes API, JSON Documents
 '''
 
 import json
@@ -12,16 +14,27 @@ from nltk.corpus import stopwords
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+'''
+A helper function to save data as a JSON Document
+'''
 def save_to_file(data, filename):
     with open(filename, 'w') as write_file:
         json.dump(data, write_file, indent = 2)
 
+'''
+A helper function to read data from a JSON Document and return the data as a dictionary
+'''
 def read_from_file(filename):
     with open(filename, 'r') as read_file:
         data = json.load(read_file)
     return data
 
-def pull_api(topic):
+'''
+A function that uses NYTimes Top Stories API to pull the top stories from a given topic
+@param topic - the topic the user would like to query
+@returns - the articles in JSON format
+'''
+def pull_api_top_stories(topic):
     api_key_dict = read_from_file("JSON_Files/api_key.json")
     api_key = api_key_dict["my_key"]
 
@@ -33,8 +46,32 @@ def pull_api(topic):
 
     return read_from_file("JSON_Files/response.json")
 
+'''
+A function that uses NYTimes Most Popular API to retrieve the most popular articles based on views, shares, or emails from 
+the last day, week, or month.
+@param article_type - the most popular articles based on either views, shares, or emails
+@param time_period - the most popular articles from the last day (1), week (7), or month (30)
+@returns - the articles in JSON format
+'''
+def pull_api_most_popular(article_type, time_period):
+    api_key_dict = read_from_file("JSON_Files/api_key.json")
+    api_key = api_key_dict["my_key"]
+
+    url = "https://api.nytimes.com/svc/mostpopular/v2/" + article_type + "/" + time_period + ".json?api-key=" + api_key
+
+    response = requests.get(url).json()
+
+    save_to_file(response, "JSON_Files/response.json")
+
+    return read_from_file("JSON_Files/response.json")
+
+'''
+A helper function that retrieves all of the words in the article's abstract section
+@param topic - the topic selected by the user
+@returns - a list of words without stop words or punctuation marks
+'''
 def get_words(topic):
-    my_article = pull_api(topic)
+    my_article = pull_api_top_stories(topic)
 
     abstract_words = ""
 
@@ -58,11 +95,21 @@ def get_words(topic):
 
     return clean_words
 
+'''
+A helper function that creates a frequency distribution of the top ten most common words from a given article
+@param topic - the topic selected by the user
+@returns - a list of tuples with the top ten words and their corresponding occurrences
+'''
 def freq_dist(topic):
     words = get_words(topic)
     fdist = FreqDist(words)
     return fdist.most_common(10)
 
+'''
+A helper function that creates the line graph figure to display the frequency distribution
+@param word_dist - the list of the top ten most common words
+@returns - the pyplot line graph figure
+'''
 def create_figure(word_dist):
     occurrences = []
     words = []
@@ -81,8 +128,12 @@ def create_figure(word_dist):
 
     return fig
 
-def create_wordcloud(topic):
-    my_article = pull_api(topic)
+'''
+A helper function that creates the wordcloud for the top stories
+@param topic - the topic selected by the user
+'''
+def create_wordcloud_top_stories(topic):
+    my_article = pull_api_top_stories(topic)
 
     abstract_words = ""
 
@@ -94,4 +145,24 @@ def create_wordcloud(topic):
     plt.figure(figsize=(12,12))
     plt.imshow(wordcloud)
     plt.axis("off")
-    plt.savefig("Images/wordcloud.png")
+    plt.savefig("Images/top_articles_wordcloud.png")
+
+'''
+A helper function that creates the wordcloud for the most popular articles
+@param article_type - the most popular articles based on either views, shares, or emails
+@param time_period - the most popular articles from the last day (1), week (7), or month (30)
+'''
+def create_wordcloud_most_popular(article_type, time_period):
+    my_article = pull_api_most_popular(article_type, time_period)
+
+    abstract_words = ""
+
+    for i in my_article["results"]:
+        abstract_words = abstract_words + i["abstract"]
+
+    wordcloud = WordCloud().generate(abstract_words)
+
+    plt.figure(figsize=(12,12))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.savefig("Images/most_popular_wordcloud.png")
